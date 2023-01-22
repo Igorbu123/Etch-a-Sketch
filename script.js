08e7490c0584f9444657f7e73435b05dda58477b
Igorbu123 <igorbulka123@gmail.com>
2023-01-22 08:48:43 +0100

    added funcition that creates default 16x16 board

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 let gridItem = document.createElement("div");
+gridItem.className = "gridItem";
 
 const playingBoard = document.querySelector(".playingBoard");
 
 //playingBoard.append(gridItem);
 
+//Creates default 16x16 playing board//
+
 function createBoard(num) {
-  for (let i = 0; i < 5; i++) {
-    playingBoard.append(gridItem);
+  for (let i = 0; i < 256; i++) {
+    let gridItem = document.createElement("div");
+    gridItem.className = "gridItem";
+    playingBoard.appendChild(gridItem);
   }
 }
 
